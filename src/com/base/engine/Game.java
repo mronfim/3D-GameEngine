ed627e6b21e81d996ebbea0566643c13c6972c3e
mronfim <mronfim@Matheus-PC>
2015-10-04 13:21:49 -0400

    Added Mesh, Util, and Vertex classes. Modified Game to draw a triangle.

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -5,9 +5,17 @@ import org.lwjgl.input.Mouse;
 
 public class Game
 {
+	private Mesh mesh;
+	
     public Game()
     {
-        
+        mesh = new Mesh();
+		
+		Vertex[] data = new Vertex[] {	new Vertex(new Vector3f(-1, -1, 0)),
+										new Vertex(new Vector3f( 0,  1, 0)),
+										new Vertex(new Vector3f( 1, -1, 0))};
+		
+		mesh.addVertices(data);
     }
     
     public void input()
@@ -30,6 +38,6 @@ public class Game
     
     public void render()
     {
-        
+        mesh.draw();
     }
 }
