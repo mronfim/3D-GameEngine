7a4ec984f539cc89c81650b57ffef25579910b6f
mronfim <mronfim@gmail.com>
2015-10-03 22:16:01 -0400

    Creating a window.

---
+++ src/com/base/game/MainComponent.java
@@ -0,0 +1,49 @@
+package com.base.game;
+
+public class MainComponent
+{
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "3D Engine";
+    
+    public MainComponent()
+    {
+        
+    }
+    
+    public void start()
+    {
+        run();
+    }
+    
+    public void stop()
+    {
+        
+    }
+    
+    public void run()
+    {
+        while(!Window.isCloseRequested())
+        {
+            render();
+        }
+    }
+    
+    public void render()
+    {
+        Window.render();
+    }
+    
+    public void cleanUp()
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        Window.createWindow(WIDTH, HEIGHT, TITLE);
+        
+        MainComponent game = new MainComponent();
+        game.start();
+    }
+}
