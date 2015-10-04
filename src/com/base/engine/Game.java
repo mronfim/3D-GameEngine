302ad8390c49e788084ab70d258561f53b3eacbf
mronfim <mronfim@Matheus-PC>
2015-10-04 14:31:15 -0400

    Added uniforms.

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -22,6 +22,8 @@ public class Game
 		shader.addVertexShader(ResourceLoader.loadShader("basicVertex.vs"));
 		shader.addFragmentShader(ResourceLoader.loadShader("basicFragment.fs"));
 		shader.compileShader();
+		
+		shader.addUniform("uniformFloat");
     }
     
     public void input()
@@ -37,9 +39,13 @@ public class Game
 			System.out.println("We've just released right mouse button!");
     }
     
+	float temp = 0.0f;
+	
     public void update()
     {
-        
+        temp += Time.getDelta();
+		
+		shader.setUniformf("uniformFloat", (float)Math.abs(Math.sin(temp)));
     }
     
     public void render()
